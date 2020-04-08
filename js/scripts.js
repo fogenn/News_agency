507dbbfd5149bcac668f0a0710ef48dd3f30fdc7
fogenn <fogennnn@gmail.com>
2020-04-08 16:17:15 +0500

    fix + add price currencies

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,32 @@
 $(document).ready(function () {
 
-  // <блок функций>
-  function getRates() {
+  let rub_eur = 0,
+      rub_usd = 0;
 
-    $.get(
-      'http://data.fixer.io/api/latest', {
-        'access_key': 'b2a27f6a318849d840d24156bbae4ec3',
-        'symbols': 'USD, RUB'
-      },
-      function (response) {
+  $.get(
+    'http://data.fixer.io/api/latest', 
+    {
+      'access_key': 'b2a27f6a318849d840d24156bbae4ec3',
+      'symbols': 'USD, RUB'
+    },
+    function (response) {
 
-        let rub_eur = response.rates.RUB,
-          rub_usd = response.rates.RUB / response.rates.USD;
-          ratesRub = {
-            'eur': rub_eur,
-            'usd': rub_usd
-          };
+      rub_eur = response.rates.RUB,
+      rub_usd = response.rates.RUB / response.rates.USD;
 
-          // console.log(ratesRub)
-          return ratesRub
-      }
-    );
+      $('.ads__price-curr__list').append(`
+  
+        <span class="ads__price-curr__position eur">
+          EUR ${rub_eur.toFixed(3)}
+        </span>
+        <span class="ads__price-curr__position usd">
+          USD ${rub_usd.toFixed(3)}
+        </span>
 
-  }
+      `);
 
-  // </блок функций>
+    }
 
-  let rub_eur,
-      rub_usd;
-
-  getRates();
-
-  console.log(getRates().ratesRub);
+  );
 
 });
